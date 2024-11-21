dae45366c546e319249fa887599c7717413fdbe5
khushi-mi <khushimishra55556@gmail.com>
2024-11-21 17:56:05 +0530

    initial commit

---
+++ Frontend/src/Component/Live/Live.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import Navbar from '.././Extra/Navbar';
+import Scorecard from './Scorecard';
+import "./Live.css";
+const Live = () =>{
+
+  //import { useEffect, useState } from "react";
+  
+     let API = "https://api.cricapi.com/v1/currentMatches?apikey=3c477fb2-6ee9-449d-bc21-0ffdb0667136&offset=0";
+  
+     const [user,setuser] = useState([]);
+     const [load,setload] = useState(true)
+    
+     const  fetchApiData = async (url) =>{
+       try{
+         const res = await fetch(url);
+         const data = await res.json();
+         console.log(data)
+         console.log(load)
+         setload(false)
+         setuser(data.data) 
+       }
+      
+       catch(error){
+       console.log(error)
+       console.log("sdcgasnkjhb")
+       }
+     }
+     useEffect(()=>{
+     fetchApiData(API)
+     },[]);
+      if(load){
+        return(
+          <>
+           <div className='loadingbox'>
+    <h1 className='text-center mt-5'>Loading....</h1>
+    </div>
+          </>
+        )
+      }
+     return(
+       <>
+       <Navbar/>
+       <div className='livescore_container'>
+       <h1 className='text-center livescorehead'>Live Scorecard</h1>
+       <Scorecard detail={user}/>
+       </div>
+       </>
+     )
+     }
+  
+   export default Live;
+  
+  
+  
+  
